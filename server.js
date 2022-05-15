b3ea9bc35600d85aca6580a96b30d922c7fed84b
MahimaVeena <mahima21389@gmail.com>
2022-05-15 16:49:58 +0530

    check point 9

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require("http");
 const app = require("./app");
 const server = http.createServer(app);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8085;
 
 server.listen(PORT, () => {
   console.log(`server listening on port ${PORT}`);
